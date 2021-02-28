ec733467b263ba4ac2ceb5e313625efc14782fa1
Maximilian <max.blade.hansen@gmail.com>
2021-02-28 14:20:43 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import App from './Containers/App';
+//import CardList from './CardList';
+import 'tachyons';
+
+//import {robots} from './robots';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
